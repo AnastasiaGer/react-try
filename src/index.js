7ed95f4edf388fe5ba7370810e078c16ce0c473e
AnastasiaGer <aagermanova@gmail.com>
2021-02-24 18:15:41 -0800

    prep final

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App/App.jsx';
+
+const root = document.querySelector(`#root`);
+
+ReactDOM.render(
+  <App />,
+  root
+);
